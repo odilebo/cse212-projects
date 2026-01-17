6cb57c8c60115f2f7153d25d1e47abc51e09955e
odilebo <odilebo279@gmail.com>
2026-01-17 19:28:42 +0200

    array complete

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+         // create a list that uses the length given by the user
+        double[] multiples = new double[length];
+
+        // loop through the list
+        for (int i = 0; i < length; i++)
+        {
+            //Calculate the multiple
+            multiples[i] = number * (i + 1);
+        }
+
+        // return the list
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +40,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // split the list
+        int splitIndex = data.Count - amount;
+
+        // create a new list containing the last amount elements.
+        List<int> tail = data.GetRange(splitIndex, amount);
+
+        // Remove the last amount elements from the original list.
+        data.RemoveRange(splitIndex, amount);
+
+        // Add the saved elements at the beginning of the list.
+        data.InsertRange(0, tail);
+
+        
     }
 }
